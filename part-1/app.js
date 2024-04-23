dc35402143e89a0c29f92a156c816fb1576e57fe
MONIKA <127090137+monika3131@users.noreply.github.com>
2024-04-23 16:27:43 +0530

    Update app.js

--- part-1/app.js
+++ part-1/app.js
@@ -1,10 +1,3 @@
-// const container = document.getElementById('react-container')
-
-// ReactDOM.render("Hello! I am CDN.", container);
-// render(make)("what to render" (i.e Hello...) & "where to render" (container));
-
-// Functional Component
-
 const Container = () => {
     return React.createElement(`div`, null, `Hey Kalvians! welcome to React Learning!`,
     React.createElement(`div`, null, `Let's rock and roll`)
@@ -14,16 +7,3 @@ const Container = () => {
 
 const container = document.getElementById('react-container');
 ReactDOM.render(React.createElement(Container), container);
-
-// Class Component
-// class ReactContainer extends ReactComponent{
-//     // Constructor Here
-
-//     render(){
-//         return ReactContainer.createElement(`div`, null, `Hey Kalvians! welcome to React Learning!`,
-//             React.createElement(`div`, null, `Let's rock and roll`)
-//             );
-//     }
-// }
-// const container = document.getElementById('react-container');
-// ReactDOM.render(React.createElement(Container), container);
