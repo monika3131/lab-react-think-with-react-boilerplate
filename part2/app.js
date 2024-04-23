022e5acc53683a24aa94ab64fe7eb75ea00275cc
MONIKA <127090137+monika3131@users.noreply.github.com>
2024-04-23 16:28:58 +0530

    Update app.js

--- part2/app.js
+++ part2/app.js
@@ -1,10 +1,3 @@
-// const container = document.getElementById('react-container')
-
-// ReactDOM.render("Hello! I am CDN.", container);
-// render(make)("what to render" (i.e Hello...) & "where to render" (container));
-
-
-//  Class Component
 class ReactContainer extends React.Component{
     // JSX tags
     // JSX there should be one parent element
